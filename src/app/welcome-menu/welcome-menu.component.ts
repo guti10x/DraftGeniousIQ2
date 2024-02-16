0c4da7520b8b7828bed679762fc7f5392a90fd43
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-02-16 23:14:23 +0100

    Add menu de opciones de págia de bienvenida

---
+++ src/app/welcome-menu/welcome-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'welcome-menu',
+  standalone: true,
+  imports: [WelcomeMenuComponent],
+  templateUrl: './welcome-menu.component.html',
+  styleUrl: './welcome-menu.component.css'
+})
+export class WelcomeMenuComponent {
+
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WelcomeMenuComponent } from '../welcome-menu/welcome-menu.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [WelcomeMenuComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
