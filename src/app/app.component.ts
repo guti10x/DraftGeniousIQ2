6e85efad4d30126357055cb1b50347623454b54f
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-02 11:29:13 +0100

    Add vetana Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LandingPageComponent} from './landing-page/landing-page.component'
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent,HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
