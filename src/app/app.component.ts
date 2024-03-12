5936f6e502fc6eaf9b30f0025c44459f8b63894f
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-13 00:24:45 +0100

    Add admin-page + fix estilos banner infinito de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
-
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent,HomePageComponent],
+  imports: [RouterOutlet, LandingPageComponent,HomePageComponent,AdminPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
