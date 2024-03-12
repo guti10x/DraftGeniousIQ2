d5f0960cc5ee4618bcd0db71df3d1291c803c486
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-12 19:26:23 +0100

    Add estilos y variable sdinámicas al banner infinito de eventeos + fix estilos menu home y landing-page

--- src/app/infinite-event-scroll/infinite-event-scroll.component.ts
+++ src/app/infinite-event-scroll/infinite-event-scroll.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './infinite-event-scroll.component.scss'
 })
 export class InfiniteEventScrollComponent {
-
+  imageSources = ["../../assets/teams/1.png", "../../assets/teams/2.png", "../../assets/teams/3.png", "../../assets/teams/4.png", "../../assets/teams/5.png", "../../assets/teams/6.png", "../../assets/teams/7.png", "../../assets/teams/8.png", "../../assets/teams/9.png", "../../assets/teams/10.png", "../../assets/teams/11.png", "../../assets/teams/12.png", "../../assets/teams/13.png", "../../assets/teams/14.png", "../../assets/teams/15.png", "../../assets/teams/16.png", "../../assets/teams/17.png", "../../assets/teams/18.png", "../../assets/teams/19.png", "../../assets/teams/20.png"];
+  resultados= ["1-0","2-0",,"3-0","1-1","2-2","4-0","1-1","1-4","1-2","1-1"];
 }
