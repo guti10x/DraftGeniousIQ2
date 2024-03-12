daa449ee32f9608f4031146a45a23eeb9d9c4bff
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-12 14:42:15 +0100

    Add btbn de despligue de comparación de jugaodres recomendados en el carrusel

--- src/app/carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component.ts
+++ src/app/carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { VersusJugadoresModalWindowComponent } from '../versus-jugadores-modal-window/versus-jugadores-modal-window.component';
 
 @Component({
   selector: 'carrusel-jugadores-recomendados',
   standalone: true,
-  imports: [NgbCarouselModule],
+  imports: [NgbCarouselModule, VersusJugadoresModalWindowComponent],
   templateUrl: './carrusel-jugadores-recomendados.component.html',
   styleUrl: './carrusel-jugadores-recomendados.component.scss'
 })
@@ -12,7 +13,7 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 export class CarruselJugadoresRecomendadosComponent {
   players = [
     {image: "../../assets/players/bellingham.png",name: 'Jude Bellingham',team: "../../assets/teams/10.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
-    {image: "../../assets/players/morata.png",name: 'Alvaro Morata',team: "../../assets/teams/2.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
+    {image: "../../assets/players/morata.png",name: 'Álvaro Morata',team: "../../assets/teams/2.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
     {image: "../../assets/players/mayoral.png",name: 'Borja Mayoral',team: "../../assets/teams/6.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
     {image: "../../assets/players/pedri.png",name: 'Pedri',team: "../../assets/teams/3.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33}
   ];
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuHomePageComponent } from '../menu-home-page/menu-home-page.component';
 import { CarruselJugadoresRecomendadosComponent } from '../carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component';
-import { VersusJugadoresModalWindowComponent } from '../versus-jugadores-modal-window/versus-jugadores-modal-window.component';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [MenuHomePageComponent,CarruselJugadoresRecomendadosComponent, VersusJugadoresModalWindowComponent],
+  imports: [MenuHomePageComponent,CarruselJugadoresRecomendadosComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
