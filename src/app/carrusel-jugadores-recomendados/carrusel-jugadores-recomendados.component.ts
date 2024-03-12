576fb8dc902f3aca0c8196aee40f65e904c25f08
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-12 15:45:22 +0100

    Add content y datos de la ventana emergente de comparación de jugaodores recomnedados del carrusel

--- src/app/carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component.ts
+++ src/app/carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component.ts
@@ -12,9 +12,9 @@ import { VersusJugadoresModalWindowComponent } from '../versus-jugadores-modal-w
 
 export class CarruselJugadoresRecomendadosComponent {
   players = [
-    {image: "../../assets/players/bellingham.png",name: 'Jude Bellingham',team: "../../assets/teams/10.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
-    {image: "../../assets/players/morata.png",name: 'Álvaro Morata',team: "../../assets/teams/2.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
-    {image: "../../assets/players/mayoral.png",name: 'Borja Mayoral',team: "../../assets/teams/6.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
-    {image: "../../assets/players/pedri.png",name: 'Pedri',team: "../../assets/teams/3.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33}
+    {image: "../../assets/players/bellingham.png",name: 'Jude Bellingham',team: "../../assets/teams/10.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "20.000.000 €", dato2: "dato2",goal: 3, assists:3, points: 123},
+    {image: "../../assets/players/morata.png",name: 'Álvaro Morata',team: "../../assets/teams/2.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "17.000.000 €", dato2: "dato2",goal: 10, assists:13, points: 33},
+    {image: "../../assets/players/mayoral.png",name: 'Borja Mayoral',team: "../../assets/teams/6.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "12.500.000 €", dato2: "dato2",goal: 3, assists:3, points: 33},
+    {image: "../../assets/players/pedri.png",name: 'Pedri',team: "../../assets/teams/3.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "9.000.000 €", dato2: "dato2",goal: 3, assists:3, points: 33}
   ];
 }
--- src/app/versus-jugadores-modal-window/versus-jugadores-modal-window.component.ts
+++ src/app/versus-jugadores-modal-window/versus-jugadores-modal-window.component.ts
@@ -9,11 +9,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './versus-jugadores-modal-window.component.scss'
 })
 export class VersusJugadoresModalWindowComponent {
+
+  players = [
+    {image: "../../assets/players/bellingham.png",name: 'Jude Bellingham',team: "../../assets/teams/10.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
+    {image: "../../assets/players/morata.png",name: 'Álvaro Morata',team: "../../assets/teams/2.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
+    {image: "../../assets/players/mayoral.png",name: 'Borja Mayoral',team: "../../assets/teams/6.png",position: "../../assets/positions/dl.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33},
+    {image: "../../assets/players/pedri.png",name: 'Pedri',team: "../../assets/teams/3.png",position: "../../assets/positions/mc.png", point1: 1, point2: 2, point3: 3, point4: 4, dato1: "dato1", dato2: "dato2",goal: 3, assists:3, points: 33}
+  ];
+
   constructor(private modalService: NgbModal) { }
 
   openSM(contenido: any) {
     // Use NgbModal to open the modal
-    const modalRef = this.modalService.open(contenido, { size: 'lg' }); // You can customize the modal options here
-    // Additional modal configuration or handling can be done here
+    const modalRef = this.modalService.open(contenido, { size: 'lg' });
+    
   }
 }
