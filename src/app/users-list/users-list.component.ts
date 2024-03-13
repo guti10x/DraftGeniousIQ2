0ea915036438bd3fd8d19e82fb907542534efe29
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-13 12:28:13 +0100

    fix búsqueda users + Add estilos a la tabla users

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,66 +1,71 @@
 import { Component, PipeTransform } from '@angular/core';
 import { AsyncPipe, DecimalPipe } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 
-interface Usuarios {
-  id: number;
-  name: string;
-  flag: string;
-  email: string;
-  psw: number;
-  esAdmin: string;
-  equipo: string;
-}
 
-const USUARIOS: Usuarios[] = [
-  {
-    id: 1,
-    name: 'Daniel',
-    flag: '/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png',
-    email: 'danihipli@gmail.com',
-    psw: 146989754,
-    esAdmin: 'Yes',
-    equipo: 'AcMilanessa'
-  },
-  {
-    id: 2,
-    name: 'Juan',
-    flag: '/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png',
-    email: 'juan@example.com',
-    psw: 123456,
-    esAdmin: 'No',
-    equipo: 'BarcelonaFC'
-  },
-  
-];
+interface Users {
+    id: number;
+    name: string;
+    flag: string;
+    email: string;
+    psw: string;
+    esAdmin: string;
+    equipo: string;
+  }
+  const COUNTRIES: Users[] = [
+    {
+      id: 1,
+      name: 'Daniel',
+      flag: '/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png',
+      email: 'danihipli@gmail.com',
+      psw: '146989754',
+      esAdmin: 'Yes',
+      equipo: 'AcMilanessa'
+    },
+    {
+      id: 2,
+      name: 'Borja',
+      flag: '/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png',
+      email: 'admin@example.com',
+      psw: 'adminAdmin12345',
+      esAdmin: 'No',
+      equipo: 'AdminFC'
+    },
+    
+    {
+      id: 2,
+      name: 'Andrés',
+      flag: '/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png',
+      email: 'parraes@gmail.com',
+      psw: 'andre123',
+      esAdmin: 'No',
+      equipo: 'BFC'
+    },
+  ];
 
-function search(text: string, pipe: PipeTransform): Usuarios[] {
-  return USUARIOS.filter((usuario) => {
-    const term = text.toLowerCase();
-    return (
-      usuario.name.toLowerCase().includes(term) ||
-      pipe.transform(usuario.name).includes(term) ||
-      pipe.transform(usuario.email).includes(term) ||
-      pipe.transform(usuario.equipo).includes(term) 
-    );
-  });
+function search(text: string, pipe: PipeTransform): Users[] {
+	return COUNTRIES.filter((country) => {
+		const term = text.toLowerCase();
+		return (
+			country.name.toLowerCase().includes(term)
+		);
+	});
 }
 
 @Component({
 	selector: 'users-list',
   standalone: true,
-	imports: [DecimalPipe, AsyncPipe, ReactiveFormsModule, NgbHighlight, CommonModule],
+	imports: [DecimalPipe, AsyncPipe, ReactiveFormsModule, NgbHighlight],
 	templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
 	providers: [DecimalPipe],
 })
-
 export class UserListComponent {
-	users$: Observable<Usuarios[]>;
+	users$: Observable<Users[]>;
 	filter = new FormControl('', { nonNullable: true });
 
 	constructor(pipe: DecimalPipe) {
