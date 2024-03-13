58d25ca48207a789406079309cf2532208d80a32
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-13 14:56:51 +0100

    Add admin menu

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserListComponent } from '../users-list/users-list.component';
+import { MenuAdminComponent } from '../menu-admin/menu-admin.component';
 
 @Component({
   selector: 'admin-page',
   standalone: true,
-  imports: [UserListComponent],
+  imports: [UserListComponent, MenuAdminComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
