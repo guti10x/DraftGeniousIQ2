d2930ec5ca6f31f135a89e45d9c1065574373ec6
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-14 14:10:48 +0100

    Add cotent y estilos menu desplegable User y Notificaciones

--- src/app/btn-desplegable-notificaciones/btn-desplegable-notificaciones.component.ts
+++ src/app/btn-desplegable-notificaciones/btn-desplegable-notificaciones.component.ts
@@ -9,6 +9,19 @@ import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './btn-desplegable-notificaciones.component.scss'
 })
 export class BtnDesplegableNotificacionesComponent {
+  notifications: string[] = [
+    "Notification received 1",
+    "Notification received 2",
+    "Notification received 3",
+    "Notification received 4",
+    "Notification received 5",
+    "Notification received 6",
+    "Notification received 7",
+    "Notification received 8",
+    "Notification received 9",
+    "Notification received 10"
+  ];
+  
   private offcanvasService = inject(NgbOffcanvas);
 
   openEnd(content: TemplateRef<any>) {
--- src/app/btn-desplegable-user/btn-desplegable-user.component.ts
+++ src/app/btn-desplegable-user/btn-desplegable-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { Component, inject, TemplateRef, OnInit } from '@angular/core';
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -12,5 +12,42 @@ export class BtnDesplegableUserComponent {
 
   openEnd(content: TemplateRef<any>) {
     this.offcanvasService.open(content, { position: 'end' });
+  }
+  users = {
+    name: ["Juan Pérez", "María García", "Pedro López"],
+    email: ["juan@example.com", "maria@example.com", "pedro@example.com"],
+    psw: ["contraseña123", "maria456", "pedro789"]
+  };
+
+  original_psw = "";
+
+  mostrarCensurada: boolean = false;
+
+  constructor() { }
+
+  alternarMostrarContrasena(): void {
+    this.mostrarCensurada = !this.mostrarCensurada;
+    if (this.mostrarCensurada) {
+      this.mostrarContrasenaCensurada(this.users.psw[0]);
+    } else {
+      this.mostrarContrasena();
+    }
+  }
+
+  mostrarContrasenaCensurada(contrasena_a_censurar: string) {
+    this.original_psw = this.users.psw[0];
+    this.users.psw[0] = contrasena_a_censurar.replace(/./g, '*');
+  }
+
+  mostrarContrasena() {
+    this.users.psw[0] = this.original_psw;
+  }
+
+  actualizarContrasena() {
+    console.log("Actualizar:", this.users.psw[0]);
+  }
+
+  ngOnInit(): void {
+    this.alternarMostrarContrasena();
   }
 }
