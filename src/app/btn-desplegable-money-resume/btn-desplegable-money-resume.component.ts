a1493c4550cc86ae427f623339288c45849accd2
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-14 10:14:44 +0100

    fix btn desplegable de balance de dinero + Update estilos botones

--- src/app/btn-desplegable-money-resume/btn-desplegable-money-resume.component.ts
+++ src/app/btn-desplegable-money-resume/btn-desplegable-money-resume.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'btn-money_resume',
   standalone: true,
-  imports: [NgbPopoverModule],
+  imports: [NgbDropdownModule],
   templateUrl: './btn-desplegable-money-resume.component.html',
   styleUrl: './btn-desplegable-money-resume.component.scss',
   host: { class: 'd-block' },
 })
+
 export class BtnDesplegableMoneyResumeComponent {
   money_bank ="2.550.001";
+  transacciones: string[] = ['trans1 €', 'trans2 €', 'trans3 €', 'trans4 €', 'trans5 €', 'trans5 €']
 }
