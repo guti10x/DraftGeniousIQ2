2eab82ea0423fd970cf1e3d58b6ab634147c46ae
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-17 00:11:28 +0100

    Add content y navegación ventana registro + fix estilos btns login y register

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomePageComponent } from '../home-page/home-page.component';
 import { AdminPageComponent } from '../admin-page/admin-page.component';
+import { RegisterPageComponent } from '../register-page/register-page.component';
 
 @Component({
   selector: 'login-page',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HomePageComponent, AdminPageComponent],
+  imports: [RouterLink, RouterOutlet, HomePageComponent, AdminPageComponent,RegisterPageComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() { }
 }
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from '../login-page/login-page.component';
+import { HomePageComponent } from '../home-page/home-page.component';
+import { AdminPageComponent } from '../admin-page/admin-page.component';
 
 @Component({
   selector: 'register-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, LoginPageComponent, HomePageComponent, AdminPageComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  user: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor() { }
 
 }
