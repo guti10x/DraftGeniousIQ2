4c71758cbce554fe5de2a3c6b4c7be878dafd297
Yul Cardenas <yulcardaso@outlook.com>
2024-03-17 05:03:47 +0100

    carousel-event update

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,11 +3,12 @@ import { MenuHomePageComponent } from '../menu-home-page/menu-home-page.componen
 import { CarruselJugadoresRecomendadosComponent } from '../carrusel-jugadores-recomendados/carrusel-jugadores-recomendados.component';
 import { InfiniteEventScrollComponent } from '../infinite-event-scroll/infinite-event-scroll.component';
 import { SoccerFieldComponent } from '../soccer-field/soccer-field.component';
+import { CarouselComponent} from '../carousel/carousel.component';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [MenuHomePageComponent,CarruselJugadoresRecomendadosComponent,InfiniteEventScrollComponent, SoccerFieldComponent],
+  imports: [MenuHomePageComponent,CarruselJugadoresRecomendadosComponent,InfiniteEventScrollComponent, SoccerFieldComponent,CarouselComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
