0998141438234938c88b16cb69d2364cdf0f8f2f
Yul Cardenas <yulcardaso@outlook.com>
2024-03-17 23:44:55 +0100

    update footer

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuLandingPageComponent } from '../menu-landing-page/menu-landing-page.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [MenuLandingPageComponent],
+  imports: [MenuLandingPageComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
